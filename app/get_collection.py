797c77282416898383a3788a695aa77e5ee88652
spink.eth <42603236+SamPink@users.noreply.github.com>
2022-06-17 11:58:22 +0100

    added sales script in app

---
+++ app/get_collection.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+import time
+import json
+import os
+
+import sales as sales
+
+slug_name = "solana_monkey_business"
+
+
+# make folder for slug in data if not exists
+if not os.path.exists(f"data/{slug_name}"):
+    os.makedirs(f"data/{slug_name}")
+
+transactions = sales.get_sales(slug_name)
+
+transactions.to_csv(f"data/{slug_name}/transactions.csv")
---
+++ app/sales.py
@@ -0,0 +1,49 @@
+import http.client
+import pandas as pd
+import time
+
+NUM_SALES = 30000
+LIMIT = 100
+
+
+def get_request(slug, offset):
+    conn = http.client.HTTPSConnection("api-mainnet.magiceden.dev")
+    payload = ""
+    headers = {}
+
+    conn.request(
+        "GET",
+        f"/v2/collections/{slug}/activities?offset={offset}&limit={LIMIT}&type=buyNow",
+        payload,
+        headers,
+    )
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode("utf-8")
+    return data
+
+
+def get_sales(slug):
+    offset = 0
+    search = True
+    all_trades = pd.DataFrame()
+    while search:
+        try:
+            sales_page = get_request(slug=slug, offset=offset)
+
+            df = pd.read_json(sales_page)
+
+            all_trades = all_trades.append(df)
+
+            offset += LIMIT
+
+            time.sleep(1)
+        except Exception as e:
+            print(e)
+            search = False
+
+        # limit to 100 calls
+        if offset > NUM_SALES:
+            search = False
+
+    return all_trades
